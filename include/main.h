6fc189464c4ea48454fe1d3c90f00bdf397d2334
NamDHay <nguyennamhuy2003@gmail.com>
2024-07-01 23:00:26 +0700

    feat : Add HTML path

--- include/main.h
+++ include/main.h
@@ -4,4 +4,21 @@
 #include <Wifi_Config.h>
 #include <File_System.h>
 
+String header;
+String HTML = "\
+<!DOCTYPE html>\
+<html>\
+<body><CENTER>\
+<h2>Cai dat Wifi</h2>\
+<form action=\"/get\">\
+  <label for=\"fname\">ten wifi:</label><br>\
+  <input type=\"text\" id=\"ssid\" name=\"ssid\" placeholder=\"ten wifi\"><br>\
+  <label for=\"lname\">mat khau wifi:</label><br>\
+  <input type=\"text\" id=\"pass\" name=\"pass\" placeholder=\"pass wifi\"><br><br>\
+  <input type=\"submit\" value=\"Luu\">\
+</form> </CENTER>\
+</body>\
+</html>\
+";
+
 #endif
